d6583788ab665b29ad4af8aae6f76b695b31ee75
PauloAnt <pauloabf235@gmail.com>
2024-02-29 10:17:27 -0300

    Spring Initializr

---
+++ src/main/java/com/praticlejava/workshopprat/WorkshoppratApplication.java
@@ -0,0 +1,13 @@
+package com.praticlejava.workshopprat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoppratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoppratApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praticlejava/workshopprat/WorkshoppratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticlejava.workshopprat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoppratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
